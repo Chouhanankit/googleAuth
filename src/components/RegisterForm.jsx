fa0df1549188812fb17878a8b4c83bf5d3361c2c
Ankit Chouhan <99067327+Chouhanankit@users.noreply.github.com>
2024-05-05 16:21:23 +0530

    title change

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -96,8 +96,8 @@ const RegisterForm = () => {
                             <div className="d-flex justify-content-center text-bg-primary">
                                 <div className="col-12 col-xl-9">
                                     <hr className="border-primary-subtle mb-4" />
-                                    <h2 className="h1 mb-4">We make digital products that drive you to stand out.</h2>
-                                    <p className="lead mb-5">We write words, take photos, make videos, and interact with artificial intelligence.</p>
+                                    <h2 className="h1 mb-4">Please register an account with Google</h2>
+                                    <p className="lead mb-5">Google Authenticator is a project developed by Google to provide an extra layer of security for online accounts.</p>
                                     <div className="text-endx">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-grip-horizontal" viewBox="0 0 16 16">
                                             <path d="M2 8a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm3 3a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm0-3a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
